d6223932c39181c31a660a31fb69dee58679eb98
Andrej <36649703+Andrej1993@users.noreply.github.com>
2020-10-18 23:37:35 +0200

    update updateDetails.php

--- updateDetails.php
+++ updateDetails.php
@@ -5,7 +5,7 @@ include("includes/includedFiles.php");
 <div class="userDetails">
 
 	<div class="container borderBottom">
-		<h2>EMAIL</h2>
+		<h2>E-MAIL</h2>
 		<input type="text" class="email" name="email" placeholder="Email address..." value="<?php echo $userLoggedIn->getEmail(); ?>">
 		<span class="message"></span>
 		<button class="button" onclick="updateEmail('email')">SAVE</button>
